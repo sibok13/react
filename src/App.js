88b2958983228d8ba964c331a663df35d397619b
Sibok <maelstrom@mail.ru>
2022-10-19 23:10:46 +0400

    01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import {Message} from './components/Message';
+import './index.css';
 
 function App() {
+
+  const textMessage = "Hello!";
+
   return (
     <div className="App">
-
+      <Message text={textMessage} />
     </div>
   );
 }
